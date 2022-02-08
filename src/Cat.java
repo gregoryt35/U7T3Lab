09085ea5a9d39a07bc24b743383e30ead37778fa
BT_1E10_31 <sample@bths.edu>
2022-02-08 10:50:24 -0500

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,17 @@
+// copy/paste this class in your project
+
+public class Cat
+{
+    private String name;
+
+    public Cat(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
+
